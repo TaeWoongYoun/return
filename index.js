9883ac49a0a58fb33bfe218cb14da6c064f717e3
xodnd0507 <24457545yong@gmail.com>
2024-02-21 17:19:36 +0900

    test : Add ms converter

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
 // return은 함수 종료 기능을 가짐
 // toFixed를 사용하면 문자로 출력됨
-console.log(vat(55555));
+// console.log(vat(55555));
 
-function vat(a){
-    var num =  (a * 1.1).toFixed(1);
-    return parseFloat(num)
+// function vat(a){
+//     var num =  (a * 1.1).toFixed(1);
+//     return parseFloat(num)
+// }
+
+// Q1. 함수에 분과 초를 차례로 파라미터로 입력하면 ms단위로 바꿔서 뱉어주는 함수를 만들어봅시다.
+// (1초 == 1000ms 입니다)
+
+console.log(함수(1,30));
+console.log(함수(2,0));
+
+function 함수(a,b){
+    var num = (a * 60000 + b * 1000).toFixed(0);
+    return parseFloat(num);
 }
